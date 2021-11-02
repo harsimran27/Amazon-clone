e0eb80227a227f07b3de2f3ed8ca445314eeb7ae
Harsimran singh <simran.9971290436@gmail.com>
2021-11-02 16:52:43 +0530

    create routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
+let App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <h1>this is a checkOut page</h1>
+          </Route>
+          <Route path="/login">
+            <h1>this is a login page</h1>
+          </Route>
+          <Route path="/">
+            <h1>this is a home page</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBbk79mMqvDRzluSzSPJfevq-2YzKEIiXs",
+    authDomain: "clone-272e3.firebaseapp.com",
+    projectId: "clone-272e3",
+    storageBucket: "clone-272e3.appspot.com",
+    messagingSenderId: "1070154383111",
+    appId: "1:1070154383111:web:eb387c082dbc974d28cfa2"
+};
+
+export default firebaseConfig;
