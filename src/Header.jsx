99583e04e67a4e2d24d690eb476f246326947c01
Harsimran singh <simran.9971290436@gmail.com>
2021-11-04 00:08:13 +0530

    complete navbar

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./css/Header.css";
 import { Link } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function Header() {
   return (
@@ -14,11 +15,40 @@ function Header() {
         />
       </Link>
 
-        <div className="header_search">
-            <input type="text" className="header_searchInput" />
-            <SearchIcon className="header_searchIcon" />
-        </div>
+      <div className="header_search">
+        <input type="text" className="header_searchInput" />
+        <SearchIcon className="header_searchIcon" />
+      </div>
 
+      <div className="header_nav">
+        <Link to="/login" className="header_Link">
+          <div className="header_option">
+            <span className="header-optionOne">Hello Guest</span>
+            <span className="header_optionTwo">Sign In</span>
+          </div>
+        </Link>
+
+        <Link to="/" className="header_Link">
+          <div className="header_option">
+            <span className="header-optionOne">Returns</span>
+            <span className="header-optionTwo">& orders</span>
+          </div>
+        </Link>
+
+        <Link to="/" className="header_Link">
+          <div className="header_option">
+            <span className="header-optionOne">Your</span>
+            <span className="header-optionTwo">Prime</span>
+          </div>
+        </Link>
+
+        <Link to="/checkout" className="header_Link">
+          <div className="header_optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header_optionTwo header_basketCount">0</span>
+          </div>
+        </Link>
+      </div>
     </nav>
   );
 }
